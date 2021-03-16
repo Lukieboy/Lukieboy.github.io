7be119ecaafcb43756705d5c5e4b2c9f893171d7
Lukieboy <56494310+Lukieboy@users.noreply.github.com>
2021-03-16 10:23:15 -0400

    Delete School-meet-links directory

--- School-meet-links/app.js
+++
@@ -1,8 +0,0 @@
-const modal = document.querySelector('.modal');
-const btn = document.querySelector('.btn');
-const modalTitle = document.querySelector('.modal-title');
-
-btn.addEventListener('click', (e)=>{
-    modalTitle.textContent = 'Launch Meeting?';
-});
-
